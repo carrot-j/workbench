c6d6682c0ef2ef67b3fa826bb8e7ebf3ba897d26
JaewonKil <kiljw316@gmail.com>
2026-05-06 01:31:32 +0900

    feat: bootstrap Spring Boot workbench service

---
+++ src/main/java/cloud/carrotj/workbench/WorkbenchApplication.java
@@ -0,0 +1,12 @@
+package cloud.carrotj.workbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbenchApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(WorkbenchApplication.class, args);
+    }
+}
