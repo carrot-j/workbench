8c64438c107e9f256aaf26831d79fa241db167e2
kiljw316 <kiljw316@gmail.com>
2026-05-08 19:18:16 +0900

    feat: base 엔티티 @Getter 추가

--- src/main/java/cloud/carrotj/workbench/entity/BaseEntity.java
+++ src/main/java/cloud/carrotj/workbench/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package cloud.carrotj.workbench.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -9,7 +10,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
-
+@Getter
 @MappedSuperclass
 public abstract class BaseEntity {
 
